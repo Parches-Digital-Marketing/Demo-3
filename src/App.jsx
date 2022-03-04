d94668ed2c1567484903d8ffccce182d212f86f5
Gabriel Caramés <49535753+GabrielCarames@users.noreply.github.com>
2022-03-04 19:44:29 -0300

    limpiada

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 import { Link } from 'react-scroll';
-import AboutUs from "./sections/AboutUs";
-import Contact from "./sections/Contact";
-import Gallery from "./sections/Gallery";
-import Footer from "./sections/Footer";
 import Navbar from "./sections/Navbar";
-import Plans from "./sections/Plans";
 import Home from "./sections/Home";
 
 function App() {
@@ -15,11 +10,6 @@ function App() {
             <Link activeClass="active" className="list__link" to="home" spy={true} isDynamic={true} smooth={true} duration={500}><button className="scroll-top"><i className="fas fa-arrow-up"></i></button></Link>
             <div className="body">
                 <Home />
-                <AboutUs />
-                <Gallery />
-                <Plans />
-                <Contact />
-                <Footer />
             </div>
         </div>
     )
--- src/sections/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <div className="footer-container">
-            <span className="copyright">©Copyright 2022 <span className="copyright__bold">Parches Digital Marketing</span>. Todos los derechos reservados. </span>
-        </div>
-    )
-}
