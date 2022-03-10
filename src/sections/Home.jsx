ae62ad04e1d32060ccda1a61ea69641f5e316bd0
Manu <mcabral@frba.utn.edu.ar>
2022-03-10 20:37:38 -0300

    added gallery animation

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,16 +1,16 @@
-import Carousel from "../components/Carousel"
+import Gallery from '../components/Gallery';
 
 function Home() {
-    return (
-        <div className="home-container">
-            <div className="scroll" name="home" />
-            <div className="home">
-                <div className="gallery">
-                    <Carousel/>
-                </div>
-            </div>
+  return (
+    <div className="home-container">
+      <div className="scroll" name="home" />
+      <div className="home">
+        <div className="gallery">
+          <Gallery />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
